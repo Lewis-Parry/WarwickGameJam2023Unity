a0b37403d1d9aba56555a9c071c527a780a2328a
Breadifies <jomds222@gmail.com>
2023-02-07 02:55:51 +0000

    Revert "Added an audio manager + slam force"
    This reverts commit edd224f29475bec3e4d10b3f7899508c79fa808f.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -75,7 +75,6 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, playerStats.jumpingPower + playerStats.speed * 0.01f); //y velocity changes
             playerStats.currentJumps -= 1;
-            FindObjectOfType<AudioManager>().Play("Jump");
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -86,9 +85,8 @@ public class PlayerMovement : MonoBehaviour
         if (vertical == -1 && playerData.slamUnlocked &&  !IsGrounded())
         {
 
-            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + fallingStrength);
-            rb.AddForce(-fallingStrength * 100f * Vector2.down);
-            FindObjectOfType<AudioManager>().Play("SlamBoomp");
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + fallingStrength);
+            downSoundEffect.Play();
         }
 
 
@@ -96,7 +94,6 @@ public class PlayerMovement : MonoBehaviour
         //DASHING
         if (Input.GetButtonDown("Dash") && canDash && playerData.dashUnlocked)
         {
-            FindObjectOfType<AudioManager>().Play("dash");
             StartCoroutine(Dash());
         }
 
